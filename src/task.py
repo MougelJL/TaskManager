fa4ff93f799baaa1095398db6187bdfdb27e5546
Lautaro Noguera <mougeljl@gmail.com>
2025-02-21 23:43:43 -0300

    Deleted the Id feature for redundace in the TaskList class

--- src/task.py
+++ src/task.py
@@ -1,28 +1,29 @@
 import datetime
 from .task_status import TaskStatus
 
+"""
+NOTE: Date and time in task features are yet to be implemented, the comments are just placeholders
+NOTE: ID was removed from the class
+"""
 class Task:
     def __init__(self, name: str = None, status = TaskStatus.TO_DO) -> None:
                  
-        #Data and meta
-        self.Id: int = None
+        #self.id: int = None
         self.name: str = name
-        self.status: TaskStatus = status       
-        self.creation_date: datetime.date = ...
-        self.update_date: datetime.date = ...
+        self.status: TaskStatus = status   
+
         
     def __str__(self):
-        return f'Name: {self.name}\nStatus: {self.status.value}'    
+        return f'Name: {self.name}\nStatus: {self.status.value}\nID: {self.id}'    
+
+    def __eq__(self, other: 'Task') -> bool:
+        return self.name == other.name and self.status == other.status
+    
+    def is_empty (self) -> bool:
+        return self.name == None or self.name == ""
+         
 
     #Getters and setters
-    @property
-    def Id(self) -> int:
-        return self._Id
-    
-    @Id.setter
-    def Id(self, value:int) -> None:
-        self._Id = value
-
     @property
     def name(self) -> str:
         return self._name
@@ -38,43 +39,3 @@ class Task:
     @status.setter
     def status(self, value: str) -> None:
         self._status = value
-
-    @property
-    def creation_date(self) -> datetime.date:
-        return self._creation_date
-
-    @creation_date.setter
-    def creation_date(self, value: datetime.date) -> None:
-        self._creation_date = value
-
-    @property
-    def update_date(self) -> datetime.date:
-        return self._update_date
-
-    @update_date.setter
-    def update_date(self, value: datetime.date) -> None:
-        self._update_date = value
-
-    @property
-    def parent_task(self) -> 'Task':
-        return self._parent_task
-
-    @parent_task.setter
-    def parent_task(self, value: 'Task') -> None:
-        self._parent_task = value
-
-    @property
-    def children_task(self) -> 'Task':
-        return self._children_task
-
-    @children_task.setter
-    def children_task(self, value: 'Task') -> None:
-        self._children_task = value
-
-    @property
-    def id(self) -> int:
-        return self._id
-
-    @id.setter
-    def id(self, value: int) -> None:
-        self._id = value
